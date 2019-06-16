09643f755b017cd01ee59a07e0fe5aca65a58869
NomeSobrenome <manavitorino@gmail.com>
2019-06-15 21:33:55 -0300

    Updates

---
+++ precache-manifest.2d167ac58d1c0a53abb1eb4f79d2331a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3619565f7bb7b6775c879be2a836d09",
+    "url": "/reactWeb/index.html"
+  },
+  {
+    "revision": "d34380d99504f61d4853",
+    "url": "/reactWeb/static/css/main.0203e4fe.chunk.css"
+  },
+  {
+    "revision": "bf2c7e2ebdf758301180",
+    "url": "/reactWeb/static/js/2.bfee90c9.chunk.js"
+  },
+  {
+    "revision": "d34380d99504f61d4853",
+    "url": "/reactWeb/static/js/main.4664cb5a.chunk.js"
+  },
+  {
+    "revision": "9b46700924b34c874a7f",
+    "url": "/reactWeb/static/js/runtime~main.1585222f.js"
+  }
+]);
