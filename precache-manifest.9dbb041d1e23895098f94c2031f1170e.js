a5874bf9ccbffc0211e17fdea2ca074f2e6425d4
Manassés <manavitorino@gmail.com>
2019-06-16 15:46:52 -0300

    project

---
+++ precache-manifest.9dbb041d1e23895098f94c2031f1170e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729605529054e2806f15e3385a49f375",
+    "url": "/reactWeb/index.html"
+  },
+  {
+    "revision": "b448ca864a8849f68cf4",
+    "url": "/reactWeb/static/css/main.0203e4fe.chunk.css"
+  },
+  {
+    "revision": "bf2c7e2ebdf758301180",
+    "url": "/reactWeb/static/js/2.bfee90c9.chunk.js"
+  },
+  {
+    "revision": "b448ca864a8849f68cf4",
+    "url": "/reactWeb/static/js/main.8990323b.chunk.js"
+  },
+  {
+    "revision": "9b46700924b34c874a7f",
+    "url": "/reactWeb/static/js/runtime~main.1585222f.js"
+  }
+]);
